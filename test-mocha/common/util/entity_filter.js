d6587cf5170a819b1ce5d51819b022d140749cfe
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-11-25 11:15:10 -0800

    Add entity filter

---
+++ test-mocha/common/util/entity_filter.js
@@ -0,0 +1,88 @@
+import entityFilter from '../../../js/common/util/entity_filter.js';
+
+const assert = require('assert');
+
+describe('EntityFilter', () => {
+  // case 1
+  it('passes all when no filters passed in', () => {
+    const filter = entityFilter();
+
+    assert(filter('sensor.test'));
+    assert(filter('sun.sun'));
+    assert(filter('light.test'));
+  });
+
+  // case 2
+  it('allows whitelisting entities by entity id', () => {
+    const filter = entityFilter(null, ['light.kitchen']);
+
+    assert(filter('light.kitchen'));
+    assert(!filter('light.living_room'));
+  });
+
+  it('allows whitelisting entities by domain', () => {
+    const filter = entityFilter(['switch']);
+
+    assert(filter('switch.bla'));
+    assert(!filter('light.kitchen'));
+  });
+
+  // case 3
+  it('allows blacklisting entities by entity id', () => {
+    const filter = entityFilter(null, null, null, ['light.kitchen']);
+
+    assert(!filter('light.kitchen'));
+    assert(filter('light.living_room'));
+  });
+
+  it('allows blacklisting entities by domain', () => {
+    const filter = entityFilter(null, null, ['switch']);
+
+    assert(!filter('switch.bla'));
+    assert(filter('light.kitchen'));
+  });
+
+  // case 4a
+  it('allows whitelisting domain and blacklisting entity', () => {
+    const filter = entityFilter(['switch'], null, null, ['switch.kitchen']);
+
+    assert(filter('switch.living_room'));
+    assert(!filter('switch.kitchen'));
+    assert(!filter('sensor.bla'));
+  });
+
+  it('allows whitelisting entity while whitelisting other domains', () => {
+    const filter = entityFilter(['switch'], ['light.kitchen']);
+
+    assert(filter('switch.living_room'));
+    assert(filter('light.kitchen'));
+    assert(!filter('sensor.bla'));
+  });
+
+  // case 4b
+  it('allows blacklisting domain and whitelisting entity', () => {
+    const filter = entityFilter(null, ['switch.kitchen'], ['switch']);
+
+    assert(filter('switch.kitchen'));
+    assert(!filter('switch.living_room'));
+    assert(filter('sensor.bla'));
+  });
+
+  it('allows blacklisting domain and excluding entities', () => {
+    const filter = entityFilter(null, null, ['switch'], ['light.kitchen']);
+
+    assert(!filter('switch.living_room'));
+    assert(!filter('light.kitchen'));
+    assert(filter('sensor.bla'));
+  });
+
+  // case 4c
+  it('allows whitelisting entities', () => {
+    const filter = entityFilter(null, ['light.kitchen']);
+
+    assert(filter('light.kitchen'));
+    assert(!filter('switch.living_room'));
+    assert(!filter('light.living_room'));
+    assert(!filter('sensor.bla'));
+  });
+});
