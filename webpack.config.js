5f51a797402bf278933130720207e4d9a86a6e76
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-05-09 21:34:12 -0400

    WIP

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+// const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/home-assistant.js',
+  },
+  plugins: [
+    // new HTMLWebpackPlugin({
+    //   title: 'Code Splitting'
+    // })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist/webpack')
+  }
+};
